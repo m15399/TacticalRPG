5189eec321bcbbfd454b81774d1e5d8d64330e78
Mark Gardner <Mark@Macintosh-6.local>
2014-04-09 12:20:09 -0700

    Added some more functionality to the GameObject class and wrote some more tests

--- src/model/GameObject.java
+++ src/model/GameObject.java
@@ -6,43 +6,92 @@ import java.util.List;
 
 public class GameObject {
 	private List<GameObject> children;
-	
-	public GameObject(){
+	private GameObject parent;
+
+	public GameObject() {
 		/*
 		 * Initializing private instance variables here
 		 */
 		children = new ArrayList<GameObject>();
+		parent = null;
 	}
-	
-	public void addChild(GameObject object){
+
+	public final void addChild(GameObject object) {
 		children.add(object);
+		object.parent = this;
 	}
-	
-	public void removeChild(GameObject object){
+
+	public final void removeChild(GameObject object) {
 		children.remove(object);
+		object.parent = null;
+	}
+
+	/*
+	 * Remove object from the tree
+	 */
+	public final void destroy() {
+		onDestroy();
+		
+		if (parent != null) {
+			parent.removeChild(this);
+		}
 	}
 	
-	public void destory(){
-		//TODO implement
+	public void onDestroy(){
+		// override
 	}
-	
-	public void update(){
-		//TODO implement
+
+	public void update() {
+		// override
 	}
-	
-	public void draw(Graphics g){
-		//TODO implement
+
+	public void draw(Graphics g) {
+		// override
 	}
 	
 	/*
+	 * Recursively update tree of objects
+	 */
+	public final void updateSelfAndChildren() {
+		// update self
+		update();
+
+		// update children recursively
+		int numChildren = children.size();
+		if (numChildren > 0) {
+			// its possible for the child to remove itself from the list while
+			// it's updating, so we have to make a copy of the list first
+			ArrayList<GameObject> childrenCopy = new ArrayList<GameObject>(children);
+			for (int i = 0; i < numChildren; i++) {
+				childrenCopy.get(i).updateSelfAndChildren();
+			}
+		}
+	}
+
+	public final void drawSelfAndChildren(Graphics g) {
+		// draw self
+		draw(g);
+
+		// draw children recursively
+		int numChildren = children.size();
+		for (int i = 0; i < numChildren; i++) {
+			children.get(i).drawSelfAndChildren(g);
+		}
+	}
+
+	/*
 	 * Setters and Getters for private instance variables.
 	 */
-	
-	public List<GameObject> getChildren(){
+
+	public final List<GameObject> getChildren() {
 		return children;
 	}
-	
-	public void setChildren(List<GameObject> newChildren){
+
+	public final void setChildren(List<GameObject> newChildren) {
 		children = newChildren;
+	}
+
+	public final GameObject getParent() {
+		return parent;
 	}
 }
--- src/tests/GameObjectTest.java
+++ src/tests/GameObjectTest.java
@@ -1,6 +1,10 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
 import model.GameObject;
 
 import org.junit.Test;
@@ -24,5 +28,91 @@ public class GameObjectTest {
 		game.removeChild(two);
 		assertFalse(game.getChildren().size() == 3);
 		assertTrue(game.getChildren().size() == 2);
+	}
+	
+	private class TestObject extends GameObject{
+		int updates;
+		int draws;
+		boolean destroyDuringUpdate;
+		boolean destroyedSuccessfully;
+		
+		public TestObject(){
+			updates = 0;
+			draws = 0;
+			destroyDuringUpdate = false;
+			destroyedSuccessfully = false;
+		}
+		
+		public void update(){
+			updates++;
+			if(destroyDuringUpdate)
+				destroy();
+		}
+		
+		public void draw(Graphics g){
+			draws++;
+		}
+		
+		public void onDestroy(){
+			destroyedSuccessfully = true;
+		}
+	}
+	
+	@Test
+	public void testUpdatingAndDrawing(){
+		GameObject root = new GameObject();
+		
+		TestObject object1 = new TestObject();
+		root.addChild(object1);
+		
+		TestObject object2 = new TestObject();
+		object1.addChild(object2);
+		
+		assertEquals(object1.updates, 0);
+		assertEquals(object2.updates, 0);
+		root.updateSelfAndChildren();
+		assertEquals(object1.updates, 1);
+		assertEquals(object2.updates, 1);
+		
+		assertEquals(object1.draws, 0);
+		assertEquals(object2.draws, 0);
+		root.drawSelfAndChildren(null);
+		assertEquals(object1.draws, 1);
+		assertEquals(object2.draws, 1);
+
+	}
+	
+	@Test
+	public void testDestroyingObjectDuringUpdate(){
+		GameObject root = new GameObject();
+		
+		ArrayList<TestObject> objects = new ArrayList<TestObject>();
+		
+		for(int i = 0; i < 10; i++){
+			TestObject o = new TestObject();
+			objects.add(o);
+			root.addChild(o);
+		}
+		
+		objects.get(0).destroyDuringUpdate = true;
+		objects.get(1).destroyDuringUpdate = true;
+		objects.get(2).destroyDuringUpdate = true;
+		
+		root.updateSelfAndChildren();
+		root.updateSelfAndChildren(); 
+		
+		assertEquals(root.getChildren().size(), 7);
+		
+		for(int i = 0; i < 10; i++){
+			TestObject o = objects.get(i);
+						
+			if(o.destroyDuringUpdate){
+				assertEquals(o.updates, 1);
+				assertTrue(o.destroyedSuccessfully);
+			} else 
+				assertEquals(o.updates, 2); 
+		}
+		
+
 	}
 }
