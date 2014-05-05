00f96b8a592475acb008963f059d2f9e00a8d3c0
dmb1 <dmb1@email.arizona.edu>
2014-05-05 13:34:33 -0700

    Eliminated redundant method call in WarpGateShip.

--- src/specific_ships_items/WarpGateShip.java
+++ src/specific_ships_items/WarpGateShip.java
@@ -9,10 +9,8 @@ public class WarpGateShip extends Ship {
 
 	public WarpGateShip(Point newLocation){
 		super(newLocation);
-		
 		String description = "Faster-than-light\ntransport.";
 		this.constructorAid("WarpGate", 0, 100, 20, 100, 20, 0, description, 0, 0, 0, 0, false, 0);
-		setVisual(new WarpVisual(this));
 	}
 	
 	@Override
