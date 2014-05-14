a03e6a192bd165f04cb2ce9abb139e97d6660ddd
emangosing@email.arizona.edu <Emango@192.168.1.101>
2014-05-13 23:18:45 -0700

    Added new song for mission 3 and reverts back to space_clips when credits appear.

--- src/levels/Mission3.java
+++ src/levels/Mission3.java
@@ -1,9 +1,11 @@
 package levels;
 
+import songPlayer.EndOfSongEvent;
+import songPlayer.EndOfSongListener;
 import songPlayer.SongPlayer;
+import model.Game.ObjectWaitingForSongToEnd;
 import specific_ships_items.Mothership;
 import level_intros.Credits;
-import model.Game.ObjectWaitingForSongToEnd;
 import model.GameObject;
 import model.Level;
 import model.Ship;
@@ -19,15 +21,24 @@ public class Mission3 extends Level {
 		super("maps/mission3");
 		
 		SongPlayer.stopFile();
-		ObjectWaitingForSongToEnd waiter = new ObjectWaitingForSongToEnd();
+		ObjectWaitingForSongToEnd3 waiter = new ObjectWaitingForSongToEnd3();
 		SongPlayer.playFile(waiter, System.getProperty("user.dir")
 			      + System.getProperty("file.separator") + "songfiles"
-			      + System.getProperty("file.separator") + "space_clips.mp3");
+			      + System.getProperty("file.separator") + "mission3.mp3");
 		
 		startTurn(0);
 
 	}
 	
+	private static class ObjectWaitingForSongToEnd3 implements EndOfSongListener {
+		public void songFinishedPlaying(EndOfSongEvent eosEvent) {
+			System.out.print("Finished " + eosEvent.fileName());
+			ObjectWaitingForSongToEnd3 waiter = new ObjectWaitingForSongToEnd3();
+			SongPlayer.playFile(waiter, System.getProperty("user.dir")
+				      + System.getProperty("file.separator") + "songfiles"
+				      + System.getProperty("file.separator") + "mission3.mp3");
+	    }
+	  }
 	
 	public void onUnitDestroyed(Ship s){
 		if(s instanceof Mothership){
@@ -37,8 +48,14 @@ public class Mission3 extends Level {
 	}
 	
 	public GameObject getNextRoot(){
-		if(getWinner() == 0)
+		if(getWinner() == 0) {
+			SongPlayer.stopFile();
+			ObjectWaitingForSongToEnd waiter2 = new ObjectWaitingForSongToEnd();
+			SongPlayer.playFile(waiter2, System.getProperty("user.dir")
+				      + System.getProperty("file.separator") + "songfiles"
+				      + System.getProperty("file.separator") + "space_clips.mp3");			
 			return new Credits();
+		}
 		else
 			return new Mission3();
 	}
